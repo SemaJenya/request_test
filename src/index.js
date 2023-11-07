e5fb838deefe41f20c27b5c4344b19f2fd8d5b38
Evgeniia Semushina <semajenya96@gmail.com>
2023-11-08 01:03:41 +0200

    added menu for mobile v

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './styles/index.css';
 
-console.log('Hello, World!') 
+const headerMenuButton = document.getElementById('header-menu');
+const headerMenu = document.querySelector('.header__nav_menu');
+const headerMenuList = document.querySelector('.header__nav');
+
+
+
+function handleOpenHeaderMenu () {
+
+    const imgMenu = document.getElementById('menu');
+    const imgCross = document.getElementById('cross');
+
+    if(headerMenu.classList.contains('header__nav_mobile')) {
+        headerMenu.classList.remove('header__nav_mobile');
+        headerMenuList.classList.remove('header__nav_list-mobile');
+        headerMenu.classList.add('header__nav_menu');
+        console.log('удалили класс');
+        imgMenu.hidden = false;
+        imgCross.hidden = true;
+    }
+    else {
+        headerMenu.classList.add('header__nav_mobile');
+        headerMenuList.classList.add('header__nav_list-mobile');
+        headerMenu.classList.remove('header__nav_menu');
+        imgMenu.hidden = true;
+        imgCross.hidden = false;
+
+        console.log('добавили класс');
+    }
+
+}
+
+
+headerMenuButton.addEventListener('click', function () {
+    handleOpenHeaderMenu();
+})
+
+
+ console.log('0000000');
